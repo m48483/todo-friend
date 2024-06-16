1e595d82ba65e5450b7608bf8e95b8c897dba404
143xx <71882579+m48483@users.noreply.github.com>
2024-06-16 16:06:55 +0900

    [feat] UserService 구현 및 테스트 완료

--- src/main/java/com/example/todo_friend/service/RequestListServiceImpl.java
+++ src/main/java/com/example/todo_friend/service/RequestListServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.todo_friend.service;
 
 import com.example.todo_friend.global.entity.RequestList;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+@Service
+@RequiredArgsConstructor
 public class RequestListServiceImpl implements RequestListService{
     @Override
     public Mono<RequestList> sendRequest(Long senderId, Long receiverId) {
