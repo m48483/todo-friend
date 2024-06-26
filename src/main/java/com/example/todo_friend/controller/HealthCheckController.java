26816949ff3b3275e6ead2f5abae147e11219644
m48483 <71882579+m48483@users.noreply.github.com>
2024-06-26 19:09:26 +0900

    database url 수정

---
+++ src/main/java/com/example/todo_friend/controller/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.example.todo_friend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "UP";
+    }
+}
